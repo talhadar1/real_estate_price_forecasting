fdab5562798b690ba92af4074e6cf794547e5f3b
talhadar1 <tal.hadar@gmail.com>
2020-01-01 11:17:13 +0200

    main  - get data from .csv file

---
+++ main.py
@@ -0,0 +1,11 @@
+#%%
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+%matplotlib inline
+
+# %%
+train = pd.read_csv(r'C:\Users\Tal\Desktop\seminar\data\TransactionsXLSsince2008orig.csv',encoding='oem')
+train.head()
+
+# %%
